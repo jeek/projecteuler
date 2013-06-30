50cd8505ba4be4d2da9bbb539e7373cdc2622213
T.J. Eckman <jeek@jeek.net>
2013-06-30 00:10:51 -0400

    palindrome.py is complete and passing pylint.

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,10 @@
+"""
+Check to see if a given number is a palindrome.
+"""
 from reverseint import reverseint
 
 def is_palindrome(x):
+    """If the number equals its reverse, it is a palindrome."""
     if x == reverseint(x):
         return True
     return False
