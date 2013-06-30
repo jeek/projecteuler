ac7054730bfb051801814459e00134c6eb07b26b
T.J. Eckman <jeek@jeek.net>
2013-06-30 01:40:31 -0400

    Added own gcd function in case gmpy is not available.

--- gcd.py
+++ gcd.py
@@ -1,4 +1,11 @@
 """Use gmpy's gcd for now."""
 
 # pylint: disable=W0611
-from gmpy import gcd
+try:
+    from gmpy import gcd
+except ImportError:
+    def gcd(a, b):
+        """Return the greatest common denominator."""
+        while b != 0:
+            (a, b) = (b, a % b)
+        return b
