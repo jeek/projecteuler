20110bde4e2ba924a6d419254769b37b37fdda34
T.J. Eckman <jeek@jeek.net>
2013-06-30 00:08:36 -0400

    reverseint passes pylint. No tests currently exist.

--- reverseint.py
+++ reverseint.py
@@ -1,7 +1,13 @@
+"""Reverse an integer."""
+
 def reverseint(x):
+    """
+    Form a new integer by lopping off the end of the current one, one
+    digit at a time.
+    """
     y = x
     z = 0
     while y > 0:
-       z = z * 10 + y % 10
-       y /= 10
+        z = z * 10 + y % 10
+        y /= 10
     return z
