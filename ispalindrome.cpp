2bce508310dd325172aa4aa82175e2623c9de5ea
T.J. Eckman <jeek@jeek.net>
2014-01-21 05:25:24 +0000

    Changed while loop from int to boolean in ispalindrome.cpp

--- ispalindrome.cpp
+++ ispalindrome.cpp
@@ -1,7 +1,7 @@
 bool ispalindrome(int original) {
     int result = 0;
     int number = original;
-    while (number) {
+    while (number > 0) {
         result *= 10;
         result += (number % 10);
         number /= 10;
