3472db401b0582425b17bf080cbf3e57f7a409a0
T.J. Eckman <jeek@jeek.net>
2014-03-22 09:51:58 +0000

    Committing old code

---
+++ py/problem208b.py
@@ -0,0 +1,16 @@
+class Memoize: # stolen from http://code.activestate.com/recipes/52201/
+    """Memoize(fn) - an instance which acts like fn but memoizes its arguments
+       Will only work on functions with non-mutable arguments
+    """
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+    def __call__(self, *args):
+        if not self.memo.has_key(args):
+            self.memo[args] = self.fn(*args)
+        return self.memo[args]
+
+
+solve = Memoize(solve)
+
+print solve(25, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
