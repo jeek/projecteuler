eca0bb11149eaf74f7ef3fc94ab43011227bdf4f
T.J. Eckman <jeek@jeek.net>
2014-04-12 07:31:52 -0400

    Update divisors.py

--- py/divisors.py
+++ py/divisors.py
@@ -1,2 +1,2 @@
 def divisors(x):
-     return sorted(list(set([i for i in range(1, 1 + int(x ** .5)) if x % i == 0] + [x / i for i in range(2, 1 + int(x ** .5)) if x % i == 0])))
+    return sorted(list(set([i for i in range(1, 1 + int(x ** .5)) if x % i == 0] + [x / i for i in range(2, 1 + int(x ** .5)) if x % i == 0])))
