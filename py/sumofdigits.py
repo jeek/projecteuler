29eed7e9d63fa2d69c8de257bdad05600ab68f42
T.J. Eckman <jeek@jeek.net>
2014-05-18 14:53:16 -0400

    updating for python3
    python3 doesn't automatically round integer division

--- py/sumofdigits.py
+++ py/sumofdigits.py
@@ -2,5 +2,5 @@ def sumofdigits(argument = 0):
     answer = 0
     while argument > 0:
         answer += argument % 10
-        argument /= 10
+        argument = int(argument / 10)
     return answer
